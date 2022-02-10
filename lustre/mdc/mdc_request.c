days0102/mmwbc

LU-14393 protocol: basic batching processing framework
Batching processing can obtain boost performace. The larger the
batch size, the higher the latency for the entire batch. Although
the latency for the entire batch of operations is higher than the
latency of any single operation, the throughput of the batch of
operations is much high.

This patch implements the basic batching processing framework for
Lustre. It could be used for the future batching statahead and
WBC.

Signed-off-by: Qian Yingjin <qian@ddn.com>
Change-Id: Idaa814e82c968811bdda1c750b18c878b2c2ca67

lustre/mdc/mdc_request.c
@@ -2938,6 +2938,10 @@ static const struct md_ops mdc_md_ops = {
 	.m_intent_getattr_async = mdc_intent_getattr_async,
 	.m_revalidate_lock      = mdc_revalidate_lock,
 	.m_rmfid		= mdc_rmfid,
+	.m_batch_create		= mdc_batch_create,
+	.m_batch_stop		= mdc_batch_stop,
+	.m_batch_flush		= mdc_batch_flush,
+	.m_batch_add		= mdc_batch_add,
 };
 
 dev_t mdc_changelog_dev;

inevity/lustre

lustre/mdc/mdc_request.c
@@ -2986,6 +2986,10 @@ static const struct md_ops mdc_md_ops = {
 	.m_intent_getattr_async = mdc_intent_getattr_async,
 	.m_revalidate_lock      = mdc_revalidate_lock,
 	.m_rmfid		= mdc_rmfid,
+	.m_batch_create		= cli_batch_create,
+	.m_batch_stop		= cli_batch_stop,
+	.m_batch_flush		= cli_batch_flush,
+	.m_batch_add		= mdc_batch_add,
 };
 
 dev_t mdc_changelog_dev;