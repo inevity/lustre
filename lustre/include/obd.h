days0102/mmwbc

LU-13021 wbc: add aging keep cache flush mode for WBC
This flush mode WBC_FLUSH_AGING_KEEP is similar to aging_drop
flush mode. Instead, the WBC EX lock for the root WBC directory
never drops actively unless it has to in the following cases:
- The client needs to revoke the cached root WBC EX lock when the
  directory is conflict accessing from a remote client or shrink
  the LRU locks in the client lock namespace;
- A application or a user wants to cleanup or uncache the cached
  data on the client manually.

This flush mode is the default flush node for WBC.
Before the root WBC EX lock is revoked, nearly all operations for
the files or directories under the root WBC directory can be
executed locally such as open()/close(), getattr(), setattr(),
readdir(), read(), write().
But for the unlink() operation, it should be handled specially
when the files are marked with Sync(s) state. In this patch, it
performs unlink synchronously on MDT for the files in the WBC
state Protected(P) | Sync(S) | Complete(P).

Test-Parameters: clientdistro=el8.3 testlist=sanity-wbc,sanity-wbc
Test-Parameters: testlist=sanity-wbc,sanity-wbc
Signed-off-by: Qian Yingjin <qian@ddn.com>
Change-Id: I1a6adf41757058b5c6dd5815ee374f8b66ce798d

lustre/include/obd.h
@@ -827,6 +827,10 @@ static inline int it_to_lock_mode(struct lookup_intent *it)
 		return LCK_PR;
 	else if (it->it_op &  IT_GETXATTR)
 		return LCK_PR;
+	else if (it->it_op & IT_SETATTR)
+		return LCK_PR;
+	else if (it->it_op & IT_WBC_EXLOCK)
+		return LCK_EX;
 
 	LASSERTF(0, "Invalid it_op: %d\n", it->it_op);
 	return -EINVAL;
@@ -962,6 +966,8 @@ enum md_opcode {
 	MD_OP_NONE		= 0,
 	MD_OP_GETATTR		= 1,
 	MD_OP_CREATE_EXLOCK	= 2,
+	MD_OP_SETATTR_EXLOCK	= 3,
+	MD_OP_EXLOCK_ONLY	= 4,
 	MD_OP_MAX,
 };
 
@@ -1151,7 +1157,7 @@ struct md_ops {
 
 	int (*m_create)(struct obd_export *, struct md_op_data *,
 			const void *, size_t, umode_t, uid_t, gid_t,
-			cfs_cap_t, __u64, struct ptlrpc_request **);
+			cfs_cap_t, __u64, __u64, struct ptlrpc_request **);
 
 	int (*m_enqueue)(struct obd_export *, struct ldlm_enqueue_info *,
 			 const union ldlm_policy_data *, struct md_op_data *,

lustre/llite/dir.c
@@ -511,7 +511,7 @@ static int ll_dir_setdirstripe(struct dentry *dparent, struct lmv_user_md *lump,
 	err = md_create(sbi->ll_md_exp, op_data, lump, len, mode,
 			from_kuid(&init_user_ns, current_fsuid()),
 			from_kgid(&init_user_ns, current_fsgid()),
-			cfs_curproc_cap_pack(), 0, &request);
+			cfs_curproc_cap_pack(), 0, 0, &request);
 	if (err)
 		GOTO(out_request, err);
 

lustre/llite/namei.c
@@ -1422,7 +1422,7 @@ again:
 	err = md_create(sbi->ll_md_exp, op_data, tgt, tgt_len, mode,
 			from_kuid(&init_user_ns, current_fsuid()),
 			from_kgid(&init_user_ns, current_fsgid()),
-			cfs_curproc_cap_pack(), rdev, &request);
+			cfs_curproc_cap_pack(), rdev, 0, &request);
 #if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 14, 58, 0)
 	/*
 	 * server < 2.12.58 doesn't pack default LMV in intent_getattr reply,

inevity/lustre

lustre/include/obd.h
@@ -833,6 +833,10 @@ static inline int it_to_lock_mode(struct lookup_intent *it)
 		return LCK_PR;
 	else if (it->it_op &  IT_GETXATTR)
 		return LCK_PR;
+	else if (it->it_op & IT_SETATTR)
+		return LCK_PR;
+	else if (it->it_op & IT_WBC_EXLOCK)
+		return LCK_EX;
 
 	LASSERTF(0, "Invalid it_op: %d\n", it->it_op);
 	return -EINVAL;
@@ -980,6 +984,8 @@ enum md_item_opcode {
 	MD_OP_NONE		= 0,
 	MD_OP_GETATTR		= 1,
 	MD_OP_CREATE_EXLOCK	= 2,
+	MD_OP_SETATTR_EXLOCK	= 3,
+	MD_OP_EXLOCK_ONLY	= 4,
 	MD_OP_MAX,
 };
 
@@ -1214,7 +1220,7 @@ struct md_ops {
 
 	int (*m_create)(struct obd_export *, struct md_op_data *,
 			const void *, size_t, umode_t, uid_t, gid_t,
-			kernel_cap_t, __u64, struct ptlrpc_request **);
+			kernel_cap_t, __u64, __u64, struct ptlrpc_request **);
 
 	int (*m_enqueue)(struct obd_export *, struct ldlm_enqueue_info *,
 			 const union ldlm_policy_data *, struct md_op_data *,

lustre/llite/dir.c
@@ -556,7 +556,7 @@ static int ll_dir_setdirstripe(struct dentry *dparent, struct lmv_user_md *lump,
 	err = md_create(sbi->ll_md_exp, op_data, lump, len, mode,
 			from_kuid(&init_user_ns, current_fsuid()),
 			from_kgid(&init_user_ns, current_fsgid()),
-			current_cap(), 0, &request);
+			current_cap(), 0, 0, &request);
 	if (err)
 		GOTO(out_request, err);
 

lustre/llite/namei.c
@@ -1643,7 +1643,7 @@ again:
 			tgt ? disk_link->len : 0, mode,
 			from_kuid(&init_user_ns, current_fsuid()),
 			from_kgid(&init_user_ns, current_fsgid()),
-			current_cap(), rdev, &request);
+			current_cap(), rdev, 0, &request);
 #if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(2, 14, 58, 0)
 	/*
 	 * server < 2.12.58 doesn't pack default LMV in intent_getattr reply,