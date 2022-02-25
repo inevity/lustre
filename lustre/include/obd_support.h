days0102/mmwbc

LU-14886 wbc: eliminate the dependency during flush
When flush a file, it must ensure that its parent has been
flushed.
The linux writeback code does not check this dependency.
In the function @writeback_sb_inodes(), when a inode (parent) is
locked for writeback (marked with I_SYNC), the kernel will skip
and move it to @b_more_io so that writeback can proceed with the
other inodes on @s_io.
However, other inodes in writeback list may have dependency on
this inode (parent) is being written back. When writeback other
inodes concurrently, it will happend the dependency problem.

Test-Parameters: clientdistro=el8.3 testlist=sanity-wbc,sanity-wbc
Test-Parameters: testlist=sanity-wbc,sanity-wbc
Signed-off-by: Qian Yingjin <qian@ddn.com>
Change-Id: I4e801741f015f5b7650b69f45dfcc7a5abc989f8

lustre/include/obd_support.h
@@ -598,6 +598,7 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_LLITE_CREATE_FILE_PAUSE2	    0x1416
 #define OBD_FAIL_LLITE_RACE_MOUNT		    0x1417
 #define OBD_FAIL_LLITE_MEMFS_LOOKUP_PAUSE	    0x1418
+#define OBD_FAIL_LLITE_WBC_FLUSH_PAUSE		    0x1419
 
 #define OBD_FAIL_FID_INDIR	0x1501
 #define OBD_FAIL_FID_INLMA	0x1502

inevity/lustre

lustre/include/obd_support.h
@@ -608,6 +608,7 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_LLITE_PAGE_ALLOC		    0x1418
 #define OBD_FAIL_LLITE_OPEN_DELAY		    0x1419
 #define OBD_FAIL_LLITE_MEMFS_LOOKUP_PAUSE	    0x141a
+#define OBD_FAIL_LLITE_WBC_FLUSH_PAUSE		    0x141b
 #define OBD_FAIL_LLITE_XATTR_PAUSE		    0x1420
 
 #define OBD_FAIL_FID_INDIR	0x1501