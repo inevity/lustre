days0102/mmwbc

LU-14757 wbc: MemFS lookup fallback to Lustre lookup
In WBC lock drop flush mode, when enter into the code path
@memfs_lookup during the new file creation, and the dir (parent
inode) is flushed (WBC state becomes WBC_STATE_FL_NONE), at this
time, it will fall back into Lustre lookup path @ll_lookup_nd to
perform lookup() operation from MDT.
The old Lustre lookup path with LOOKUP_CREATE | LOOKUP_OPEN will
return ERR_PTR(-ENOENT) wrongly.

In this patch, when fallback lookup from MDT returns -ENOENT to
indicate that the name path is not existed, it will return NULL
instead of ERR_PTR(-ENOENT). Otherwise, the creation application
will report -ENOENT error.

Test-Parameters: clientdistro=el8.3 testlist=sanity-wbc,sanity-wbc
Test-Parameters: testlist=sanity-wbc,sanity-wbc
Signed-off-by: Qian Yingjin <qian@ddn.com>
Change-Id: I0955a4684dc4bca7c638c2a7716bad6efd936f94

lustre/include/obd_support.h
@@ -597,6 +597,7 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_LLITE_SHORT_COMMIT		    0x1415
 #define OBD_FAIL_LLITE_CREATE_FILE_PAUSE2	    0x1416
 #define OBD_FAIL_LLITE_RACE_MOUNT		    0x1417
+#define OBD_FAIL_LLITE_MEMFS_LOOKUP_PAUSE	    0x1418
 
 #define OBD_FAIL_FID_INDIR	0x1501
 #define OBD_FAIL_FID_INLMA	0x1502

inevity/lustre

lustre/include/obd_support.h
@@ -607,6 +607,7 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_LLITE_RACE_MOUNT		    0x1417
 #define OBD_FAIL_LLITE_PAGE_ALLOC		    0x1418
 #define OBD_FAIL_LLITE_OPEN_DELAY		    0x1419
+#define OBD_FAIL_LLITE_MEMFS_LOOKUP_PAUSE	    0x141a
 #define OBD_FAIL_LLITE_XATTR_PAUSE		    0x1420
 
 #define OBD_FAIL_FID_INDIR	0x1501